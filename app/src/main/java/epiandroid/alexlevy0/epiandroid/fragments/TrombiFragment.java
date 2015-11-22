65003a7ca33dcbbb93bec7d65e01691d8fccfdd8
alexlevy0 <alexlevy0@gmail.com>
2015-11-22 18:22:39 +0100

    add fragment + fix crash on message

--- app/src/main/java/epiandroid/alexlevy0/epiandroid/fragments/TrombiFragment.java
+++ app/src/main/java/epiandroid/alexlevy0/epiandroid/fragments/TrombiFragment.java
@@ -2,10 +2,10 @@ package epiandroid.alexlevy0.epiandroid.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import epiandroid.alexlevy0.epiandroid.R;
 
 public class TrombiFragment extends Fragment {
@@ -14,4 +14,6 @@ public class TrombiFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.trombi_fragment, container, false);
     }
+
+
 }
